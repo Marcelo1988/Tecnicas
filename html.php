7ed6a6bb5070dd5fbef61c04cb4b3028a2ad6107
marcelothuglife2@hotmail.com <marcelothuglife2@hotmail.com>
2015-04-23 17:24:20 -0500

    proyecto tecnicas

---
+++ html.php
@@ -0,0 +1,21 @@
+<?php
+echo " hoy es" . date("l"). ".";
+?>
+les presentamos las noticias de hoy
+
+<script type = "text/javascript">
+document.write("hoy es" + Date());
+</script>
+
+
+<html>
+<head>
+<title> Hola Mundo </title>
+ </head>
+ <body>  
+<BODY BGCOLOR="blue">
+<H1 align="center"><FONT color="black">Escuela Politecnica Nacional</FONT> </H1><p align="center"> <font size=6 >Marcelo Prado </font> <br/> 
+ <A HREF="http://es.wikipedia.org/wiki/Escuela_Polit%C3%A9cnica_Nacional" target="destino"><FONT color="black"><FONT color="white">Mostrar Informacion E.P.N</FONT></A><br/> <br/> 
+<Center><img src="http://2.bp.blogspot.com/-XR_5r-FHUkA/UO3FG3pFr6I/AAAAAAAAFcg/OHUmLV75MQw/s1600/escuela-politecnica-nacional.png" width="200" height="200" BORDER="0"></Center><br /><br />
+</body>
+ </html>
